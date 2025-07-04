1ae68016d56ee1addcb959430e45da7648806343
jp-lima <jpbrasil763@gmail.com>
2025-07-04 19:15:19 -0300

    javascript melhorado

--- static.js
+++ static.js
@@ -1,21 +1,34 @@
-let i = 0;
+
+
+let lista_com_ctts = [] 
+
+function criar_linhas(lista){
+    const nova_li = document.createElement('li')
+
+    nova_li.innerHTML = `${lista[0]}   ${lista[1]}` 
+
+    document.querySelector('ul').append(nova_li)
+}
 
 
 document.querySelector('#enviar').onclick = () => {
+
+console.log('hahdhdhiuahudcxs')
+
     nome = document.querySelector('#nome_do_ctt').value;
     numero = document.querySelector('#numero_do_ctt').value;
 
-    const dicionario ={ name:nome, number:numero};
 
-    console.log(dicionario);
+    let lista = [nome, numero];
 
-    return false;
-}
+    
+    lista_com_ctts.push(lista);
+    
+
+    document.querySelector('#nome_do_ctt').value = ''
+    document.querySelector('#numero_do_ctt').value = ''
+
+criar_linhas(lista);
 
 
-
-function hello(){
-for (i; i<=10; i++){
-    console.log(i);
-}
 }
